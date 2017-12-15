904659d44b7c8d0f6fc8cb9d2510c83ea01204c7
Adrián <Adravila@users.noreply.github.com>
2017-12-15 16:46:06 +0100

    Update Cleverbot for Unity

---
+++ Cleverbot Unity/Assets/Scripts/SpeechToText.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Windows.Speech;
+using UnityEngine.Windows;
+using UnityEngine.UI;
+using System.Linq;
+
+public class SpeechToText : MonoBehaviour {
+
+	KeywordRecognizer keywordRecognizer;
+	Dictionary<string,System.Action> keywords = new Dictionary<string, System.Action>();
+	private DictationRecognizer m_DictationRecognizer;
+	[SerializeField]
+
+	void Start () {
+		m_DictationRecognizer = new DictationRecognizer ();
+
+		m_DictationRecognizer.DictationResult += (cadena, confidence) => {
+			if(isActiveAndEnabled){
+				Debug.LogFormat ("Dictation result: {0}", cadena);
+				GetComponent<ChatterBotAPITest> ().Iniciar(cadena);
+				cadena += cadena + "\n";
+			}
+		};
+		m_DictationRecognizer.Start ();
+	}
+}
